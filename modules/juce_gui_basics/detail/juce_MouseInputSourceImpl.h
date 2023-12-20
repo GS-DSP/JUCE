f9cfdc4f1e9542e1c909c9d39e141700690d8949
James Swift <james@3dengineer.com>
2023-12-20 11:29:35 +0100

    Fix ios menus

--- modules/juce_gui_basics/detail/juce_MouseInputSourceImpl.h
+++ modules/juce_gui_basics/detail/juce_MouseInputSourceImpl.h
@@ -300,7 +300,8 @@ public:
         {
             cancelPendingUpdate();
 
-            lastPointerState = newPointerState;
+            if (newPointerState.position != MouseInputSource::offscreenMousePos)
+                lastPointerState = newPointerState;
 
             if (auto* current = getComponentUnderMouse())
             {
